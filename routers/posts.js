51f81c832b50b8ed2eb995a61c8f3dfa0e6bb328
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-22 01:36:57 +0200

    [UPD] Definizione rotta show

--- routers/posts.js
+++ routers/posts.js
@@ -8,3 +8,8 @@ const router = express.Router();
 router.get(`/`, (req, res) => {
     res.send(`Lista dei post`)
 });
+
+// show
+router.get(`/:id`, (req, res) => {
+    res.send(`Dettaglio del post: ` + req.params.id)
+});
